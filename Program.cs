6809dd31a53e2e580cbab36534ec18e2362b49ec
McRaser <kanapushka30@gmail.com>
2024-02-18 17:30:10 +0300

    Код для решения задачи написан

--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int CountingNumberOfStrings(string[] array)
+{
+    int number = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            number++;
+        }
+    }
+    return number;
+}
+
+string[] CreateNewArray(string[] array,int number)
+{
+  string[] secondArray = new string[null];
+  int j = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= 3) 
+    {
+     secondArray[j] = array[i];j++;
+    }
+  }
+  return secondArray;
+}
+
+static void WriteArray(string[] secondArray)
+{
+    for (int i = 0; i < secondArray.Length; i++)
+    {
+        Console.WriteLine(secondArray[i]);
+    }
+}
+
+string[] strings = { "Sun", "Monday", ".", "...", "Thirsday", "=-)", "qqqqq" };
+WriteArray(CreateNewArray (strings, CountingNumberOfStrings(strings)));
